b57c575124bb605ad88bc1eb7ff14c22f76562e6
healerfighter <palgunitr@gmail.com>
2025-06-05 00:37:59 +0530

    change

--- dos_detection_project/settings.py
+++ dos_detection_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'your-app-name.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://netdetectai-dos-1.onrender.com']
 
 
 # Static files (CSS, JavaScript, Images)
